cae2c49d28b4ca0e9e7c63cfb308a02f7d080c17
MirkoColo <colombomirko06@gmail.com>
2024-01-23 11:31:58 +0100

    Aggiungere i file di progetto.

---
+++ ClasseFrazione/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ClasseFrazione
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Num = new System.Windows.Forms.TextBox();
+            this.Den = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Somma = new System.Windows.Forms.Button();
+            this.Sottrazione = new System.Windows.Forms.Button();
+            this.Divisione = new System.Windows.Forms.Button();
+            this.Moltiplicazione = new System.Windows.Forms.Button();
+            this.Semplifica = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Num
+            // 
+            this.Num.Location = new System.Drawing.Point(35, 48);
+            this.Num.Name = "Num";
+            this.Num.Size = new System.Drawing.Size(31, 20);
+            this.Num.TabIndex = 1;
+            this.Num.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Num.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Den
+            // 
+            this.Den.Location = new System.Drawing.Point(35, 89);
+            this.Den.Name = "Den";
+            this.Den.Size = new System.Drawing.Size(31, 20);
+            this.Den.TabIndex = 2;
+            this.Den.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "----------";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Somma
+            // 
+            this.Somma.Location = new System.Drawing.Point(107, 48);
+            this.Somma.Name = "Somma";
+            this.Somma.Size = new System.Drawing.Size(31, 23);
+            this.Somma.TabIndex = 5;
+            this.Somma.Text = "+";
+            this.Somma.UseVisualStyleBackColor = true;
+            this.Somma.Click += new System.EventHandler(this.Somma_Click);
+            // 
+            // Sottrazione
+            // 
+            this.Sottrazione.Location = new System.Drawing.Point(107, 86);
+            this.Sottrazione.Name = "Sottrazione";
+            this.Sottrazione.Size = new System.Drawing.Size(31, 23);
+            this.Sottrazione.TabIndex = 6;
+            this.Sottrazione.Text = "-";
+            this.Sottrazione.UseVisualStyleBackColor = true;
+            // 
+            // Divisione
+            // 
+            this.Divisione.Location = new System.Drawing.Point(144, 86);
+            this.Divisione.Name = "Divisione";
+            this.Divisione.Size = new System.Drawing.Size(31, 23);
+            this.Divisione.TabIndex = 8;
+            this.Divisione.Text = ":";
+            this.Divisione.UseVisualStyleBackColor = true;
+            // 
+            // Moltiplicazione
+            // 
+            this.Moltiplicazione.Location = new System.Drawing.Point(144, 48);
+            this.Moltiplicazione.Name = "Moltiplicazione";
+            this.Moltiplicazione.Size = new System.Drawing.Size(31, 23);
+            this.Moltiplicazione.TabIndex = 7;
+            this.Moltiplicazione.Text = "X";
+            this.Moltiplicazione.UseVisualStyleBackColor = true;
+            // 
+            // Semplifica
+            // 
+            this.Semplifica.Location = new System.Drawing.Point(107, 115);
+            this.Semplifica.Name = "Semplifica";
+            this.Semplifica.Size = new System.Drawing.Size(68, 23);
+            this.Semplifica.TabIndex = 9;
+            this.Semplifica.Text = "Semplifica";
+            this.Semplifica.UseVisualStyleBackColor = true;
+            this.Semplifica.Click += new System.EventHandler(this.Semplifica_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 252);
+            this.Controls.Add(this.Semplifica);
+            this.Controls.Add(this.Divisione);
+            this.Controls.Add(this.Moltiplicazione);
+            this.Controls.Add(this.Sottrazione);
+            this.Controls.Add(this.Somma);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Den);
+            this.Controls.Add(this.Num);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox Num;
+        private System.Windows.Forms.TextBox Den;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Somma;
+        private System.Windows.Forms.Button Sottrazione;
+        private System.Windows.Forms.Button Divisione;
+        private System.Windows.Forms.Button Moltiplicazione;
+        private System.Windows.Forms.Button Semplifica;
+    }
+}
+
---
+++ ClasseFrazione/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClasseFrazione
+{
+    public partial class Form1 : Form
+    {
+        Frazione frazione = new Frazione();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Semplifica_Click(object sender, EventArgs e)
+        {
+            if (Inserimento())
+            {
+                frazione.Semplifica();
+                Num.Text = frazione._numeratore.ToString();
+                Den.Text = frazione._denominatore.ToString();
+            }
+        }
+        public bool Inserimento()
+        {
+            bool giusto = float.TryParse(Num.Text, out _) == true && float.TryParse(Den.Text, out _) == true && Num.Text != string.Empty && Den.Text != string.Empty;
+            if(giusto == true)
+            {
+                frazione._numeratore = float.Parse(Num.Text);
+                frazione._denominatore = float.Parse(Den.Text);
+            }
+            return giusto;
+        }
+
+        private void Somma_Click(object sender, EventArgs e)
+        {
+            Inserimento();
+            MessageBox.Show("La somma è: " + frazione.Somma());
+        }
+    }
+    class Frazione
+    {
+        public float _numeratore { get; set; }
+        public float _denominatore { get; set; }
+        public float _minore { get; set; }
+        public void TrovaMinore()
+        {
+            if (_numeratore > _denominatore)
+            {
+                _minore = _denominatore;
+            }
+            else
+            {
+                _minore = _numeratore;
+            }
+        }
+        public void Semplifica()
+        {
+            TrovaMinore();
+            for (int i = 2; i <= _minore; i++)
+            {
+                if (_numeratore % i == 0 && _denominatore % i == 0)
+                {
+                    _numeratore = _numeratore / i;
+                    _denominatore = _denominatore / i;
+                    i = 2;
+                }
+            }
+        }
+
+
+
+        
+    }
+}
